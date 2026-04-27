a4a2ff7b21342f59a7bbd3fb772547e51ed03b94
David Francoeur <dfrancoeur04@gmail.com>
2026-04-27 13:18:47 -0400

    feat: add agenix

--- installation/flake.nix
+++ installation/flake.nix
@@ -1,5 +1,7 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.agenix.url = "github:ryantm/agenix";
+  inputs.agenix.inputs.nixpkgs.follows = "nixpkgs";
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
 
@@ -7,6 +9,7 @@
     {
       nixpkgs,
       disko,
+      agenix,
       ...
     }:
     {
@@ -14,6 +17,7 @@
         system = "x86_64-linux";
         modules = [
           disko.nixosModules.disko
+          agenix.nixosModules.default
           ./configuration.nix
           ./box1/default.nix
         ];
