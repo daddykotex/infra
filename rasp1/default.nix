9f105b9f7dd8ffa48f1755f2b32e998cdb0e5c63
David Francoeur <dfrancoeur04@gmail.com>
2026-05-06 20:57:44 -0400

    feat: add user password

--- rasp1/default.nix
+++ rasp1/default.nix
@@ -36,6 +36,9 @@
     users.users.rasp1 = {
       isNormalUser = true;
       extraGroups = [ "wheel" ];
+
+      # password can be hashed with: nix run nixpkgs#mkpasswd -- -m SHA-512 -s
+      hashedPassword = "$6$/UkVdJiyFhjmvdbN$K7uLtBEJ9qHgYmUu5qG07XW3473caYKD6R9vBr4..gAdKRiNPWQ3B.ldjipB.U52Xtkj5rAvIdSyBfIjhNnUF0";
       openssh.authorizedKeys.keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCxDLOoi5qM5kz3g9HMTsNE5WPWxNo8LUpvO1QBEBKDe43gjHUcjpMMgedPWT2ShPeOiB4B7CXpiq+MoQrdM/LzR1FQstaGfIo0pwhFYLXTnPHtZg3yxVV9AjgHMyyXIwkfubtjCj9DY0y2n3IFZTxXgM+67MSGptAJHVRqNDCia//hIF8PB6/7QnGPycMSQ3DViVCE33DBNHvj2j2ywHmma5vsK15NLynXP3dBNOdv4a/dsAg+MjOvdq5Tv/EDV3V3hrDrjxFGpXUI3OAovmxeiWH7WDB2NdJj2BEfRF+hKgMAoAiLb6pNXC8rvwZ5V3m9oP7JKcfgnvEljI/KA7kDq5o5n0fEMtoARQvPP6mnicnoGyJhP8us69WI5Z8HgYbR5g325ZfWWixveiqn2ZZPKvY9+Uk6tkjIOkJWFStEHmc5uh0EbLSqmGXlVw2QyEmUDcZ4H/4JqIpm7i5VedZpUN6gG5RaGvDuV9rslfxcAgmsQ0W93un9X1YYohomcAzhQNYeVutYpHiokOXuMtsSz9OgDcxTK3vYIHscNl6QcV8PqmdlP11e4VCXItOjLddAc1gsFFQ5IfTHkZ4aqqgY1Tdl6vxFI8dPt0dyA3LCLu9mgWr7J4Ev4IdQP00AKAPf/uTCX2Bk0eR50StEQWxez7m16zCll/gDOECCjVU2yw=="
       ];
